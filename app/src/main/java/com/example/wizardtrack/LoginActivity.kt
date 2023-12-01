c6d74a96e28dbf3745c145cea0a8c77335f98191
nivak22 <90485437+nivak22@users.noreply.github.com>
2023-11-30 21:10:54 -0500

    se hace conexion de login y registro con los botones

---
+++ app/src/main/java/com/example/wizardtrack/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.wizardtrack
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+
+        val botonRegistro = findViewById<Button>(R.id.botonRegistro)
+        val botonEntrar = findViewById<Button>(R.id.btnEntrar)
+
+        botonEntrar.setOnClickListener {
+            Log.d("LoginActivity", "Clic en el botón de entrar")
+            val intent = Intent(this@LoginActivity, contentActivity::class.java)
+            startActivity(intent)
+        }
+        botonRegistro.setOnClickListener {
+            Log.d("LoginActivity", "Clic en el botón de registro")
+            val intent = Intent(this@LoginActivity, RegistroActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/wizardtrack/MainActivity.kt
+++ app/src/main/java/com/example/wizardtrack/MainActivity.kt
@@ -18,5 +18,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login) // Cambia "R.layout.login" por el nombre real de tu archivo XML "login.xml"
+
     }
 }
